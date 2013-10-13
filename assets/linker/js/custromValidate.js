73624903c6fbb306493c9c01b05f3e197111fcaa
Radek Adamus <adamus.r@gmail.com>
2013-10-13 07:03:58 +0200

    validator localization

--- assets/linker/js/custromValidate.js
+++ assets/linker/js/custromValidate.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-	$('.form-horizontal').validate({
+
+	$('.form-horizontal').validate({		
 		rules :{
+			lname: {
+				required:true,
+			},
 			phone: {
 				required:true,
 				digits:true
@@ -9,6 +13,18 @@ $(document).ready(function(){
 				email : true
 			}
 		},		
+		messages: {
+			lname: {
+				required: "Wprowadź nazwisko",
+			},
+    		phone: {
+       			required: "Książka telefoniczna bez numeru telefonu?",
+       			digits: "Wprowadź cyfry"
+     		},
+     		email: {       			
+       			email: "Wprowadź poprawny adres email"
+     		}
+   		}
 	});
 	
 })
